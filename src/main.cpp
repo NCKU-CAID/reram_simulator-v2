f493f9510cb9b11c946a89ebb3ccffc8a677c36a
cicero884 <joy861225@gmail.com>
2022-04-20 10:00:24 +0800

    Fix diagram and init main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <bitset>
+#include<cstdlib>
+#include "parser.h"
+
+FILE *fptr
+int main(int argc, char* argv[]){
+	if(argc!=3){
+		printf("usage: %s instruction_file SRAM_file",argv[0])
+		return 1;
+	}
+	double instruction;
+	while(fscanf(fptr,"%lld",&instruction)!=EOF){
+		
+	}
+}
