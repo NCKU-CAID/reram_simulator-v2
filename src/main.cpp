967f1430a866700fb996a43ca174df9f2a188500
GuFangYi <graceabc2@gmail.com>
2022-05-07 17:08:23 +0800

    Format files

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,53 @@
+#include <fstream>
+#include <iostream>
 #include "cell.h"
 #include "tile.h"
-#include <iostream>
-#include <fstream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	/* test Cell class
-	Cell testcell;
-	testcell.setVoltage(2.5);
-	testcell.setEnable(0);
-	cout << "Area = " << testcell.getArea() << "nm^2" << endl;
-	if (testcell.getCellType() == 0)
-		cout << "Cell Type = 6T SRAM" << endl;
-	else
-		cout << "Cell Type = RRAM" << endl;
-	cout << "BL = " << testcell.getVoltage() << "(V)" << endl;
-	cout << "WL = " << testcell.isWLON() << endl;
-	cout << "Stored weight = " << testcell.getValue() << endl;
-	*/
+    /* test Cell class
+    Cell testcell;
+    testcell.setVoltage(2.5);
+    testcell.setEnable(0);
+    cout << "Area = " << testcell.getArea() << "nm^2" << endl;
+    if (testcell.getCellType() == 0)
+        cout << "Cell Type = 6T SRAM" << endl;
+    else
+        cout << "Cell Type = RRAM" << endl;
+    cout << "BL = " << testcell.getVoltage() << "(V)" << endl;
+    cout << "WL = " << testcell.isWLON() << endl;
+    cout << "Stored weight = " << testcell.getValue() << endl;
+    */
 
-	Tile testtile(16,1,4);
-	cout << "Tile size = " << testtile.getSize() << "x" << testtile.getSize() << endl;
+    Tile testtile(16, 1, 4);
+    cout << "Tile size = " << testtile.getSize() << "x" << testtile.getSize()
+         << endl;
 
-	ofstream outfile("temp_weight", ios::out);
-	if(!outfile){
-		cerr << "Failed opening file" << endl;
-		exit(1);
-	}
-	outfile << 255 << "\t" << 2  << "\t" << 5 << endl
-			<< 4   << "\t" << 64 << "\t" << 10 <<endl
-			<< 25 << "\t" << 100 << "\t" << 234 << endl;
+    ofstream outfile("temp_weight", ios::out);
+    if (!outfile) {
+        cerr << "Failed opening file" << endl;
+        exit(1);
+    }
+    outfile << 255 << "\t" << 2 << "\t" << 5 << endl
+            << 4 << "\t" << 64 << "\t" << 10 << endl
+            << 25 << "\t" << 100 << "\t" << 234 << endl;
 
-	outfile << 255 << "\t" << 2  << "\t" << 5 << endl
-			<< 4   << "\t" << 64 << "\t" << 10 <<endl
-			<< 25 << "\t" << 100 << "\t" << 234 << endl;
+    outfile << 255 << "\t" << 2 << "\t" << 5 << endl
+            << 4 << "\t" << 64 << "\t" << 10 << endl
+            << 25 << "\t" << 100 << "\t" << 234 << endl;
 
-	//cout << "Created a temporary testing weight file" << endl;
+    // cout << "Created a temporary testing weight file" << endl;
 
-	ifstream inFile("temp_weight", ios::in);
-	if(!inFile){
-		cerr << "Failed opening file" << endl;
-		exit(1);
-	}
+    ifstream inFile("temp_weight", ios::in);
+    if (!inFile) {
+        cerr << "Failed opening file" << endl;
+        exit(1);
+    }
 
-	testtile.programWeights("temp_weight", 3, 3, 8);
-
-
+    testtile.programWeights("temp_weight", 3, 3, 8);
 
 
-	return 0;
+
+    return 0;
 }
